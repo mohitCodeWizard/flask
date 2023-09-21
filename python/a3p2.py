77deb0a8549b70a48a8347e4e95457ac26e0e491
mohitgithubpatel <mohitnaliydara63@gmail.com>
2023-09-21 18:01:27 +0530

    done

--- python/a3p2.py
+++ python/a3p2.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import *
 
 app = Flask(__name__)
 
---
+++ python/a3p9.py
@@ -0,0 +1,39 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("a3p9registra.html")
+
+@app.route("/display/<data>")
+def display(data):
+    return render_template("a3p9disp.html",data = data)
+
+@app.route("/addData", )
+def addData():
+    hobbie =""
+    if request.method == "GET":
+        nm = request.args['txtname']
+        email = request.args['txtemail']
+        pas = request.args['txtpass']
+        phone = request.args['txtphone']
+        gender = request.args['gender']
+        if request.args.get('ch1'):
+            hobbie += "read,"
+        if request.args.get('ch2'):
+            hobbie += "write,"
+        if request.args.get('xh3'):
+            hobbie += "paint"
+        city = request.args['txtcity']
+        addr = request.args['txtaddr']
+        
+        data =[{"name" : nm,"email": email, "pass": pas , "phone": phone , "gender": gender , "hobbie" : hobbie ,
+                "city": city , "addr":addr
+                }]
+        
+        return render_template("a3p9disp.html", data = data)
+        
+
+if __name__ == '__main__':
+    app.run(debug=True)
